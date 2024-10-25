c8fca304f572992c78888eb7ef538371db2cbb14
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-25 15:38:27 +0200

    tabella completata

--- database/migrations/2024_10_25_131210_create_trains_table.php
+++ database/migrations/2024_10_25_131210_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 255);
+            $table->string('stazione_di_partenza', 255);
+            $table->string('stazione_di_arrivo', 255);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->char('codice_treno', 12)->unique();
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
