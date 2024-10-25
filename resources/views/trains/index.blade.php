3048bf9115e509cdd5d59e7e6ec9e94d898fd578
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-25 16:16:55 +0200

    tutti i dati mostrati in pagina

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -4,6 +4,52 @@
 
 @section('main-content')
 
-    <div class="container"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="fw-bold text-center fst-italic my-5">TRENI</h1>
+            </div>
+
+            <div class="col-12">
+                <table class="table-dark table table-striped table-hover">
+                    <thead>
+                        <tr class="">
+                            <th>#</th>
+                            <th>Nome azienda</th>
+                            <th>Stazione di partenza</th>
+                            <th>Stazione di arrivo</th>
+                            <th>Orario di partenza</th>
+                            <th>Orario di arrivo</th>
+                            <th>Codice treno</th>
+                            <th>Numero di carrozze</th>
+                            <th>Corsa Cancellata</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($trains as $train)
+                            <tr>
+                                <td> {{ $train->id }} </td>
+                                <td> {{ $train->azienda }} </td>
+                                <td> {{ $train->stazione_di_partenza }} </td>
+                                <td> {{ $train->stazione_di_arrivo }} </td>
+                                <td> {{ $train->orario_di_partenza }} </td>
+                                <td> {{ $train->orario_di_arrivo }} </td>
+                                <td> {{ $train->codice_treno }} </td>
+                                <td> {{ $train->numero_carrozze }} </td>
+                                <td> {{ $train->in_orario }} </td>
+                                <td> {{ $train->cancellato }} </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td>
+                                    Nessun treno disponbile
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
