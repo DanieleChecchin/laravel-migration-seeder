005ec5526a3119b0638fb22f3a4bc2c7ef4a12da
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-28 12:21:50 +0100

    seeder

--- database/migrations/2024_10_28_110412_create_passengers_table.php
+++ database/migrations/2024_10_28_110412_create_passengers_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('passengers', function (Blueprint $table) {
             $table->id();
-            $table->string
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->tinyInteger('age');
+            $table->string('address');
             $table->timestamps();
         });
     }
